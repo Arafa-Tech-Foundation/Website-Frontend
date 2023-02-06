e8ce9b79ae3e5381a5f7f4df6f2b259f5cc79abe
Hazim Arafa <hazimarafa80@gmail.com>
2023-02-05 22:33:20 -0800

    Update legal.tsx

--- src/pages/legal.tsx
+++ src/pages/legal.tsx
@@ -1,9 +1,10 @@
+import HomeLayout from "@components/layout";
 import { NextSeo } from "next-seo";
 
 export default function Legal() {
 	return (
-		<>
+		<HomeLayout>
 			<NextSeo title="Legal" />
-		</>
+		</HomeLayout>
 	);
 }
