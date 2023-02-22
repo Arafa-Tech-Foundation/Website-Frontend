cb6d6ff735d8dcc952aff6ae031ebf5e170146a7
Hazim Arafa <hazimarafa80@gmail.com>
2023-02-22 13:49:11 -0800

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,19 +1,19 @@
-import NextAuth, { Session, User } from "next-auth";
+import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import axios from "axios";
 
 export default NextAuth({
 	providers: [
 		DiscordProvider({
-			clientId: "1072350253417762847",
+			clientId: "1073492380398923868",
 			clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
-			authorization:
-				"https://discord.com/api/oauth2/authorize?scope=identify+email+guilds.join",
+			authorization: { params: { scope: "identify email guilds.join" } },
 		}),
 	],
 	callbacks: {
 		async jwt({ token, account }) {
 			// Persist the OAuth access_token to the token right after signin
+			console.log(account);
 			if (account) {
 				token.accessToken = account.access_token;
 			}
@@ -21,30 +21,29 @@ export default NextAuth({
 		},
 		async session({ session, token }) {
 			if (!session) return session;
-
-			const newSession = session as Session & {
-				user: User & { id: string };
-				accessToken: string;
-			};
-
+			// Send properties to the client, like an access_token from a provider.
 			// @ts-ignore
-			newSession.accessToken = token.accessToken;
+			session.accessToken = token.accessToken;
 			// @ts-ignore
-			newSession.user.id = token.sub;
-
-			await axios.put(
-				`https://discord.com/api/guilds/1071904870644338739/members/${newSession.user.id}`,
-				{
-					access_token: `Bearer ${token.accessToken}`,
-				},
-				{
-					headers: {
-						Authorization: `Bot ${process.env.DISCORD_CLIENT_SECRET}`,
+			session.user.id = token.sub;
+			try {
+				if (!token.accessToken) return session;
+				await axios.put(
+					// @ts-ignore
+					`https://discord.com/api/guilds/1071904870644338739/members/${session.user.id}`,
+					{
+						access_token: `Bearer ${token.accessToken}`,
 					},
-				}
-			);
-
-			return newSession;
+					{
+						headers: {
+							Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+						},
+					}
+				);
+			} catch (e) {
+				// console.log(e);
+			}
+			return session;
 		},
 	},
 });
