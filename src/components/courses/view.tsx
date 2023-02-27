763d0a8c5007c6d9f5abf0847e393d64739c5c28
Nik Schaefer <nikkschaefer@gmail.com>
2023-02-26 21:57:25 -0600

    :recycle: Refactor Course Design on Dashboard

---
+++ src/components/courses/view.tsx
@@ -0,0 +1,51 @@
+import clsx from "clsx";
+import { CourseMeta } from "types";
+
+export default function CoursesView({
+	title,
+	description,
+	courses,
+}: {
+	title: string;
+	description: string;
+	courses: CourseMeta[];
+}) {
+	return (
+		<section className="my-4">
+			<div className="flex flex-col justify-center items-center">
+				<h1 className="text-4xl font-semibold">{title}</h1>
+				<p className="my-2">{description}</p>
+			</div>
+			<div className="container grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 my-4 mx-auto gap-6">
+				{courses.map((course) => (
+					<div
+						className={clsx(
+							"col-span-1 border-primary flex flex-col justify-center items-center h-[300px]",
+							"p-5 rounded-xl relative from-[#4a4a54] to-[#272733] bg-gradient-to-r",
+							"hover:scale-105 transition-transform duration-300 ease-in-out hover:cursor-pointer"
+						)}
+					>
+						<img
+							className="w-10 grayscale"
+							src="https://cdn.iconscout.com/icon/free/png-256/python-3521655-2945099.png"
+						/>
+						<div className="w-full flex absolute bottom-0 p-4 items-center justify-between">
+							<div>
+								<h2 className="text-lg font-bold">
+									{course.title}
+								</h2>
+								<p className="text-sm">{course.description}</p>
+							</div>
+							<h3 className="whitespace-nowrap font-semibold pl-2">
+								{Math.round(course.minutes / 60)}{" "}
+								{Math.round(course.minutes / 60) > 1
+									? "Hours"
+									: "Hour"}
+							</h3>
+						</div>
+					</div>
+				))}
+			</div>
+		</section>
+	);
+}
