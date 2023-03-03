ffb9cfd75799e89024fad555a0473dc04d0003a1
Hazim Arafa <hazimarafa80@gmail.com>
2023-03-02 19:28:27 -0800

    ✨Needed Legal Papers

--- src/pages/legal.tsx
+++ src/pages/legal.tsx
@@ -8,9 +8,18 @@ export default function Legal() {
 			<NextSeo title="Legal" />
 			<HomeLayout>
 				<Section>
-					{documents.map((document) => (
-						
-					))}
+					<h1 className="text-3xl text-center mb-10">Legal</h1>
+					<div className="grid grid-cols-3 gap-4 py-10">
+						{documents.map((document) => (
+							<a
+								href={document.href}
+								target="_blank"
+								className="grid-item bg-primary text-primary-content px-4 py-2 rounded hover:scale-105 cursor-pointer"
+							>
+								<h1 className="text-2xl">{document.title}</h1>
+							</a>
+						))}
+					</div>
 				</Section>
 			</HomeLayout>
 		</>
@@ -20,5 +29,14 @@ export default function Legal() {
 const documents = [
 	{
 		title: "Articles of Incorporation",
+		href: "/documents/articles-of-incorporation.pdf",
+	},
+	{
+		title: "Bylaws",
+		href: "/documents/bylaws.pdf",
+	},
+	{
+		title: "EIN",
+		href: "/documents/ein.pdf",
 	},
 ];
