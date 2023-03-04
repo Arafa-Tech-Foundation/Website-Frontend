ad118962be3295b48153f02e26430e8cad3e1e46
Hazim Arafa <hazimarafa80@gmail.com>
2023-03-04 12:19:09 -0500

    :wrench: Rewrite Discord Invite URL

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const config = {
 	swcMinify: true,
+	rewrites: async () => [
+		{
+			source: "/discord",
+			destination: "https://discord.gg/HQvTe2pHym",
+		},
+	],
 };
 
 module.exports = config;
