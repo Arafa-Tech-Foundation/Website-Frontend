261d115f511773528f3c32c5da7427323af32003
lmaosoggypancakes <macerajosef@gmail.com>
2023-03-09 22:16:27 -0800

    ♻️ modify course meta schema

--- src/types.ts
+++ src/types.ts
@@ -3,12 +3,16 @@ type CourseMeta = {
 	type: string;
 	languages: string[];
 	level: string;
-	order: string[];
+	schema: CourseModule[]
 	title: string;
 	description: string;
 	minutes: number; // in minutes (not seconds) :0
 };
 
+type CourseModule = {
+	name: string;
+	order: string[]
+}
 type Matter = {};
 
-export type { CourseMeta, Matter };
+export type { CourseMeta, CourseModule, Matter };
