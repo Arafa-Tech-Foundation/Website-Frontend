ec0c6be4d92dcec80c22b8fcc4c8d703994ab238
Hazim Arafa <hazimarafa80@gmail.com>
2023-03-19 02:00:46 -0700

    :sparkles: Poppins Font

--- src/components/index/hero.tsx
+++ src/components/index/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
 	return (
 		<Section>
 			<div className="flex flex-col items-center gap-8 py-20 h-[1000px]">
-				<h1 className="text-[20vw] text-center md:text-6xl lg:text-7xl xl:text-8xl leading-none select-none tracking-tightest font-extrabold flex flex-col md:flex-row">
+				<h1 className="text-[20vw] text-center md:text-6xl lg:text-7xl xl:text-8xl leading-none select-none tracking-tight font-bold flex flex-col md:flex-row">
 					<span
 						data-content="Learn."
 						className="relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-2 before:bottom-0 before:left-0 before:text-center before:bg-white/25 before:bg-clip-text before:backdrop-blur-sm before:text-transparent before:animate-gradient-background-1 "
@@ -32,7 +32,7 @@ export default function Hero() {
 					</span>
 				</h1>
 
-				<p className="text-lg sm:text-md md:text-lg lg:text-xl xl:text-2xl w-full max-w-4xl text-center">
+				<p className="sm:text-sm md:text-md lg:text-lg xl:text-xl w-full max-w-4xl text-center font-light bg-white/60 bg-clip-text backdrop-blur-sm text-transparent">
 					Arafa Tech is a non-profit organization that provides coding
 					education and mentorship to underprivileged students,
 					empowering them with technical skills for the future.
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,10 @@ export default function Document() {
 					crossOrigin=""
 				/>
 				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+					rel="stylesheet"
+				/>
+				<link
 					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
 					rel="stylesheet"
 				/>
