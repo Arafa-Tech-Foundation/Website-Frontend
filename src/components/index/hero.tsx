7420bca0e01aef2fd6c25e8278e80d2d3d6e0048
lmaosoggypancakes <macerajosef@gmail.com>
2023-03-18 22:43:30 -0700

    ♻️ lighter white on hero text

--- src/components/index/hero.tsx
+++ src/components/index/hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
 				<h1 className="text-[20vw] text-center md:text-6xl lg:text-7xl xl:text-8xl  leading-none select-none tracking-tightest font-extrabold flex flex-col md:flex-row">
 					<span
 						data-content="Learn."
-						className="relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-2 before:bottom-0 before:left-0 before:text-center before:bg-white/5 before:bg-clip-text before:backdrop-blur-sm before:text-transparent before:animate-gradient-background-1 "
+						className="relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-2 before:bottom-0 before:left-0 before:text-center before:bg-white/25 before:bg-clip-text before:backdrop-blur-sm before:text-transparent before:animate-gradient-background-1 "
 					>
 						<span className="px-2 text-transparent bg-clip-text bg-gradient-to-r from-gradient-start to-gradient-end animate-gradient-foreground-1 text-shadow">
 							Learn.
@@ -16,7 +16,7 @@ export default function Hero() {
 					</span>
 					<span
 						data-content="Develop."
-						className="relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-2 before:bottom-0 before:left-0 before:text-center before:bg-white/5 before:bg-clip-text before:backdrop-blur-sm before:text-transparent before:animate-gradient-background-2"
+						className="relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-2 before:bottom-0 before:left-0 before:text-center before:bg-white/25 before:bg-clip-text before:backdrop-blur-sm before:text-transparent before:animate-gradient-background-2"
 					>
 						<span className="px-2 text-transparent bg-clip-text bg-gradient-to-r from-gradient-start to-gradient-end animate-gradient-foreground-2 text-shadow">
 							Develop.
@@ -24,7 +24,7 @@ export default function Hero() {
 					</span>
 					<span
 						data-content="Succeed."
-						className="relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-2 before:bottom-0 before:left-0 before:text-center before:bg-white/5 before:bg-clip-text before:backdrop-blur-sm before:text-transparent before:animate-gradient-background-3"
+						className="relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-2 before:bottom-0 before:left-0 before:text-center before:bg-white/25 before:bg-clip-text before:backdrop-blur-sm before:text-transparent before:animate-gradient-background-3"
 					>
 						<span className="px-2 text-transparent bg-clip-text bg-gradient-to-r from-gradient-start to-gradient-end animate-gradient-foreground-3 text-shadow">
 							Succeed.
