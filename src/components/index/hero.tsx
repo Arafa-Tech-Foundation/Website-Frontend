72d3b3e50379e45182f6a6855623462d1de96b51
Hazim Arafa <hazimarafa80@gmail.com>
2023-03-18 23:39:41 -0700

    :bug: The P in "Develop." fixed on tranistion

--- src/components/index/hero.tsx
+++ src/components/index/hero.tsx
@@ -5,12 +5,12 @@ export default function Hero() {
 	return (
 		<Section>
 			<div className="flex flex-col items-center gap-8 py-20">
-				<h1 className="text-[20vw] text-center md:text-6xl lg:text-7xl xl:text-8xl  leading-none select-none tracking-tightest font-extrabold flex flex-col md:flex-row">
+				<h1 className="text-[20vw] text-center md:text-6xl lg:text-7xl xl:text-8xl leading-none select-none tracking-tightest font-extrabold flex flex-col md:flex-row">
 					<span
 						data-content="Learn."
 						className="relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-2 before:bottom-0 before:left-0 before:text-center before:bg-white/25 before:bg-clip-text before:backdrop-blur-sm before:text-transparent before:animate-gradient-background-1 "
 					>
-						<span className="px-2 text-transparent bg-clip-text bg-gradient-to-r from-gradient-start to-gradient-end animate-gradient-foreground-1 text-shadow">
+						<span className="px-2 text-transparent bg-clip-text bg-gradient-to-r from-gradient-start to-gradient-end animate-gradient-foreground-1 text-shadow leading-none">
 							Learn.
 						</span>
 					</span>
@@ -18,7 +18,7 @@ export default function Hero() {
 						data-content="Develop."
 						className="relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-2 before:bottom-0 before:left-0 before:text-center before:bg-white/25 before:bg-clip-text before:backdrop-blur-sm before:text-transparent before:animate-gradient-background-2"
 					>
-						<span className="px-2 text-transparent bg-clip-text bg-gradient-to-r from-gradient-start to-gradient-end animate-gradient-foreground-2 text-shadow">
+						<span className="px-2 text-transparent bg-clip-text bg-gradient-to-r from-gradient-start to-gradient-end animate-gradient-foreground-2 text-shadow leading-none">
 							Develop.
 						</span>
 					</span>
@@ -26,7 +26,7 @@ export default function Hero() {
 						data-content="Succeed."
 						className="relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-2 before:bottom-0 before:left-0 before:text-center before:bg-white/25 before:bg-clip-text before:backdrop-blur-sm before:text-transparent before:animate-gradient-background-3"
 					>
-						<span className="px-2 text-transparent bg-clip-text bg-gradient-to-r from-gradient-start to-gradient-end animate-gradient-foreground-3 text-shadow">
+						<span className="px-2 text-transparent bg-clip-text bg-gradient-to-r from-gradient-start to-gradient-end animate-gradient-foreground-3 text-shadow leading-none">
 							Succeed.
 						</span>
 					</span>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
-import Hero from "@components/index/hero";
-import Features from "@components/index/features";
 import HomeLayout from "@components/layout";
-import Courses from "@components/index/courses";
+import Hero from "@components/index/hero";
 
 export default function Home() {
 	return (
 		<HomeLayout>
 			<Hero />
-			<Features />
-			<Courses />
 		</HomeLayout>
 	);
 }
