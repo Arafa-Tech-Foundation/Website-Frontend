33d17d4d57b4c215a4840b051ad3f3ded95c9c63
Hazim Arafa <hazimarafa80@gmail.com>
2023-03-20 01:16:20 -0700

    :fire: Remove 2D shapes. Will add 3d Later

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,12 @@
 import HomeLayout from "@components/layout";
 import Hero from "@components/index/hero";
 import React from "react";
-import Shapes from "@components/index/shapes";
 import UnderConstruction from "@components/underConstruction";
 
 export default function Home() {
 	return (
 		<HomeLayout>
-			<Hero>
-				<Shapes />
-			</Hero>
+			<Hero />
 			<UnderConstruction />
 		</HomeLayout>
 	);
