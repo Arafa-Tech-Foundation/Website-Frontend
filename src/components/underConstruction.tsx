d33c70ac4e2b171792e40674937e7a76ab13648e
Hazim Arafa <hazimarafa80@gmail.com>
2023-03-19 21:08:21 -0700

    :construction: Home Page

--- src/components/underConstruction.tsx
+++ src/components/underConstruction.tsx
@@ -3,7 +3,7 @@ import Section from "./section";
 export default function UnderConstruction() {
 	return (
 		<Section>
-			<h1 className="text-5xl text-center">Under Construction...</h1>
+			<h1 className="text-5xl text-center mb-[400px]">Under Construction...</h1>
 		</Section>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import HomeLayout from "@components/layout";
 import Hero from "@components/index/hero";
+import UnderConstruction from "@components/underConstruction";
 
 export default function Home() {
 	return (
 		<HomeLayout>
 			<Hero />
+			<UnderConstruction />
 		</HomeLayout>
 	);
 }
