dff4db46568cdd47f59e742cf71650ffc72ea3eb
Hazim Arafa <hazimarafa80@gmail.com>
2023-04-05 17:56:53 -0400

    :recycle: New contact info

--- src/components/contact/card.tsx
+++ src/components/contact/card.tsx
@@ -8,7 +8,7 @@ export default function ContactCard() {
 						Need help or have a suggestion? You can shoot us an
 						email at{" "}
 						<span className="font-bold ">
-							arafa.tech.foundation@gmail.com
+							support@arafa.tech
 						</span>
 						!
 					</p>
@@ -16,27 +16,10 @@ export default function ContactCard() {
 						className="btn btn-md w-48 btn-primary mt-3"
 						onClick={() =>
 							(window.location.href =
-								"mailto:arafa.tech.foundation@gmail.com")
+								"mailto:support@arafa.tech")
 						}
 					>
 						Email Us
-					</button>
-				</div>
-
-				<div className="rounded-md flex flex-col text-center justify-center items-center max-w-sm w-full">
-					<h1 className="font-2xl font-bold mt-3">Phone</h1>
-					<p className="my-3 max-h-16 h-full">
-						You can also get in touch with us over the phone by
-						dialing <br />{" "}
-						<span className="font-bold">(702) 530-7389</span>!
-					</p>
-					<button
-						className="btn btn-md w-48 btn-primary mt-3"
-						onClick={() =>
-							(window.location.href = "tel:7025307389")
-						}
-					>
-						Call Us
 					</button>
 				</div>
 			</section>
