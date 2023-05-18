6eac9ccf3b90aac8670294d40dcae86eab53e748
Hazim Arafa <hazimarafa80@gmail.com>
2023-05-18 13:56:57 -0700

    :sparkles: Vercel Sponsorship

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -84,6 +84,11 @@ export default function Footer() {
 								</a>
 							</li> */}
 						</ul>
+						<img
+							src="https://images.ctfassets.net/e5382hct74si/78Olo8EZRdUlcDUFQvnzG7/fa4cdb6dc04c40fceac194134788a0e2/1618983297-powered-by-vercel.svg"
+							alt="Vercel Banner"
+							className="mt-4"
+						/>
 					</div>
 					<div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-3">
 						<div>
