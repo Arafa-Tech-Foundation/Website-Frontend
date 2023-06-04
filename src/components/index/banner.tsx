bc5d1169054c3e7b9638c6cb20422770d0ce8ef5
lmaosoggypancakes <macerajosef@gmail.com>
2023-06-04 12:48:34 -0700

    🐛 mobile performance + icon size

--- src/components/index/banner.tsx
+++ src/components/index/banner.tsx
@@ -10,11 +10,14 @@ export default function Banner() {
 				<Section className="bg-primary bg-opacity-80 text-center text-neutral py-2">
 					Remember to sign up for our workshops!
 					<a href="https://arafa.tech/discord">
-						<FontAwesomeIcon icon={faArrowRight} className="ml-2" />
+						<FontAwesomeIcon
+							icon={faArrowRight}
+							className="ml-2 h-4 w-4"
+						/>
 					</a>
 					<button
 						onClick={() => setShow(false)}
-						className="float-right"
+						className="float-right hidden md:block"
 					>
 						<FontAwesomeIcon icon={faX} />
 					</button>
