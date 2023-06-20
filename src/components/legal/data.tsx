b68731eb8588c57eb2ffe56cc718db932a4ad9e4
Jyan Pico <picojyan@gmail.com>
2023-06-20 08:03:10 -0400

    💩 new legal page

---
+++ src/components/legal/data.tsx
@@ -0,0 +1,39 @@
+const legalData = [
+    {
+        title: "Terms and Conditions of Use",
+        subTitle: "Arafa Tech Terms and Conditions",
+        description: `1. Introduction
+2. User Conduct
+3. Content
+4. Intellectual Property Rights
+5. Disclaimers and Limitations of Liability
+6. Termination and Suspension
+7. Governing Law and Jurisdiction
+8. Changes to the Terms and Conditions
+9. Miscellaneous
+
+<strong>1. Introduction</strong>
+Welcome to our website. By accessing and using this website, you agree to comply with these terms and conditions. These terms and conditions govern your use of our website and any content, services or features made available to you through our website. If you do not agree with these terms and conditions, please do not use our website.
+
+<strong>2. User Conduct</strong>
+You agree to use our website in compliance with applicable laws and regulations, and not to use our website for any illegal or unauthorized purpose. You also agree not to interfere with or disrupt the operation of our website, or to hack, spam or introduce any malicious code.
+
+<strong>3. Content</strong>
+All content on our website, including text, graphics, images, and logos, is the property of our website or its content suppliers and is protected by applicable intellectual property laws. You agree not to reproduce, duplicate, copy, sell, resell, or exploit any portion of our website or its content.
+
+<strong>4. Intellectual Property Rights</strong>
+All intellectual property rights, including copyrights, trademarks, and patents, in connection with our website and its content, are the exclusive property of our website or its content suppliers. You agree not to use or exploit any of our intellectual property rights without our prior written consent.`,
+    },
+    {
+        title: "Copyright & Privacy Policy",
+        subTitle: "Arafa Copyright & Privacy Policy",
+        description: `Enim culpa adipisicing incididunt dolor Lorem. Dolor laborum commodo excepteur et excepteur ea fugiat. Sint reprehenderit tempor consectetur in magna ex do cillum et do aliqua amet. Minim duis nostrud incididunt duis laboris fugiat cillum. Sit exercitation veniam fugiat veniam mollit commodo proident cupidatat incididunt quis sit sunt esse. Eu dolor nostrud minim nulla et cupidatat amet duis elit amet.`,
+    },
+    {
+        title: "User Guidelines",
+        subTitle: "Arafa User Guidelines",
+        description: `Velit anim anim exercitation culpa sint proident duis excepteur. Eu nisi dolore in labore. Elit reprehenderit proident cillum adipisicing qui fugiat veniam officia ut ullamco anim adipisicing magna. Magna cupidatat ad consequat mollit ad eiusmod ad sit labore. Consequat incididunt sint eiusmod sint ullamco laborum. Dolore deserunt Lorem ullamco minim. Nostrud excepteur et sunt elit ipsum aliquip.`,
+    },
+]
+
+export { legalData }; 
--- src/pages/legal.tsx
+++ src/pages/legal.tsx
@@ -1,25 +1,70 @@
 import HomeLayout from "@components/layout";
 import Section from "@components/section";
 import { NextSeo } from "next-seo";
+import clsx from "clsx";
+import { legalData } from "@components/legal/data";
+import { useState } from "react";
 
 export default function Legal() {
+	// Using state to change between doucments in data file
+	const [document, setDocument] = useState(legalData[0]);
+
+	const handleDocumentChange = (info: any) => {
+		setDocument(info);
+	};
+
 	return (
 		<>
 			<NextSeo title="Legal" />
 			<HomeLayout>
 				<Section>
-					<h1 className="text-3xl text-center mb-10">Legal</h1>
-					<div className="flex flex-col gap-4 md:grid md:grid-cols-3">
-						{documents.map((document) => (
-							<a
-								key={document.href}
-								href={document.href}
-								target="_blank"
-								className="grid-item bg-gradient-to-r from-primary to-secondary text-center text-primary-content px-4 py-2 rounded hover:scale-105 cursor-pointer transition"
-							>
-								<h1 className="text-2xl">{document.title}</h1>
-							</a>
-						))}
+					<div className="text-white">
+						<div className="flex">
+							<h1 className="text-2xl text-glow font-semibold mb-10 mr-5 mt-3">
+								Legal
+							</h1>
+							<div className="flex rounded-xl gap-4 mb-10 bg-neutral w-fit px-4 py-2 mx-auto">
+								{legalData.map((info) => (
+									<button
+										key={info.title}
+										className={clsx(
+											"flex flex-col text-xl items-center hover:text-primary px-2 py-1 ",
+											info.title === document.title &&
+												"text-primary bg-neutral rounded-xl"
+										)}
+										onClick={() =>
+											handleDocumentChange(info)
+										}
+									>
+										{info.title}
+									</button>
+								))}
+							</div>
+						</div>
+						<div className="whitespace-pre-wrap">
+							<div className="text-5xl font-semibold mb-5">
+								<h3>{document.subTitle}</h3>
+							</div>
+							<p
+								dangerouslySetInnerHTML={{
+									__html: document.description,
+								}}
+							></p>
+						</div>
+						<div className="flex">
+							{documents.map((document) => (
+								<a
+									key={document.href}
+									href={document.href}
+									target="_blank"
+									className="my-5 mr-5 p-2 bg-gradient-to-r from-primary to-secondary text-primary-content rounded hover:scale-105 cursor-pointer transition"
+								>
+									<h1 className="text-xl">
+										{document.title}
+									</h1>
+								</a>
+							))}
+						</div>
 					</div>
 				</Section>
 			</HomeLayout>
@@ -27,6 +72,7 @@ export default function Legal() {
 	);
 }
 
+// Old documents just appended to the bottom
 const documents = [
 	{
 		title: "Articles of Incorporation",
