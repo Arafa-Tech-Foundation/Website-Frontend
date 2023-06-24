6a3fd42e42602f91cc303773f686beadffa0c084
Nik Schaefer <nikkschaefer@gmail.com>
2023-06-23 23:53:27 -0500

    fix: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -34,73 +34,78 @@ export default function Header() {
 	const [state, setState] = useState(false);
 
 	return (
-		<nav className="bg-base-100/75 top-0 sticky backdrop-blur shadow-[0_5px_200px_-30px_hsl(var(--p))] py-2 z-[100] w-full md:static md:text-sm md:border-none">
-			<div className="items-center mx-auto md:flex">
-				<div className="flex items-center justify-between py-3 md:py-5 md:block px-10">
-					<Link className="flex items-center gap-4" href="/">
-						<span className="sr-only">Home</span>
-						<img
-							src="/logos/transparent/main.png"
-							className="max-w-[3em]"
-							alt="arafa tech's logo"
-						/>
-						<h1 className="hidden text-xl md:block font-semibold tracking-widest uppercase">
-							Arafa Tech
-						</h1>
-					</Link>
-					<div className="md:hidden">
-						<button
-							className="text-primary"
-							onClick={() => setState(!state)}
-						>
-							{state ? (
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									className="h-6 w-6"
-									viewBox="0 0 20 20"
-									fill="currentColor"
+		<div className="bg-base-100/75 backdrop-blur sticky !px-0 top-0 py-2 z-[100] shadow-[0_5px_200px_-30px_hsl(var(--p))]">
+			<nav className="w-full md:static md:text-sm md:border-none ">
+				<div className="items-center mx-auto md:flex">
+					<div className="flex items-center justify-between py-3 md:py-5 md:block px-10">
+						<Link className="flex items-center gap-4" href="/">
+							<span className="sr-only">Home</span>
+							<img
+								src="/logos/transparent/main.png"
+								className="max-w-[3em]"
+								alt="arafa tech's logo"
+							/>
+							<h1 className="hidden text-xl md:block font-semibold tracking-widest uppercase">
+								Arafa Tech
+							</h1>
+						</Link>
+						<div className="md:hidden">
+							<button
+								className="text-primary"
+								onClick={() => setState(!state)}
+							>
+								{state ? (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										className="h-6 w-6"
+										viewBox="0 0 20 20"
+										fill="currentColor"
+									>
+										<path
+											fillRule="evenodd"
+											d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+											clipRule="evenodd"
+										/>
+									</svg>
+								) : (
+									<FontAwesomeIcon size="lg" icon={faBars} />
+								)}
+							</button>
+						</div>
+					</div>
+					<div
+						className={`flex-1 pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+							state ? "block" : "hidden"
+						}`}
+					>
+						<ul className="justify-end items-center space-y-6 pr-6 md:flex md:space-x-6 md:space-y-0">
+							{headerLinks.map((item, idx) => {
+								return (
+									<li
+										key={idx}
+										className="text-gray-300 h5 hover:text-primary-600"
+									>
+										<Link
+											href={item.href}
+											className="block"
+										>
+											{item.name}
+										</Link>
+									</li>
+								);
+							})}
+							<li>
+								<Link
+									href="/discord"
+									className="btn btn-primary w-full"
 								>
-									<path
-										fillRule="evenodd"
-										d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-										clipRule="evenodd"
-									/>
-								</svg>
-							) : (
-								<FontAwesomeIcon size="lg" icon={faBars} />
-							)}
-						</button>
+									Join the Community
+								</Link>
+							</li>
+						</ul>
 					</div>
 				</div>
-				<div
-					className={`flex-1 pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-						state ? "block" : "hidden"
-					}`}
-				>
-					<ul className="justify-end items-center space-y-6 pr-6 md:flex md:space-x-6 md:space-y-0">
-						{headerLinks.map((item, idx) => {
-							return (
-								<li
-									key={idx}
-									className="text-gray-300 h5 hover:text-primary-600"
-								>
-									<Link href={item.href} className="block">
-										{item.name}
-									</Link>
-								</li>
-							);
-						})}
-						<li>
-							<Link
-								href="/discord"
-								className="btn btn-primary w-full"
-							>
-								Join the Community
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+			</nav>
+		</div>
 	);
 }
